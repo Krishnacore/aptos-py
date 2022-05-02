7a74ec36c55c46c2cf2b040c94653923fc6a9a0e
Artsiom Holikau <boogeyband@gmail.com>
2022-05-01 22:44:59 -0400

    add get api endpoints

--- aptospy/base.py
+++ aptospy/base.py
@@ -15,6 +15,11 @@ class MnemonicLanguageCode:
     KOREAN = 'ko'
     SPANISH = 'es'
 
+chain_url = {
+    'devnet': 'https://fullnode.devnet.aptoslabs.com',
+    'local': 'http://127.0.0.1:8080'
+}
+
 class Keypair:
 
     def __init__(self):
@@ -48,20 +53,81 @@ class Keypair:
         """
         return bip39_validate(mnemonic, language_code)
 
-    @classmethod
-    def create_from_mnemonic(cls, mnemonic: str,  language_code: str = MnemonicLanguageCode.ENGLISH) -> 'Keypair':
+    # @classmethod
+    # def create_from_mnemonic(cls, mnemonic: str,  language_code: str = MnemonicLanguageCode.ENGLISH) -> 'Keypair':
+    #     """
+        
+    #     """
+    #     private_key = bip39.decode_phrase(mnemonic)
+    #     hex_private_key = private_key.hex()
+    #     public_key = SigningKey(mnemonic).verify_key.encode().hex()
+
+    #     return cls(private_key=private_key, hex_private_key=hex_private_key, public_key=public_key)
+
+class Aptos:
+
+    def __init__(self, url=None, chain=None):
         """
         
         """
-        private_key = bip39.decode_phrase(mnemonic)
-        hex_private_key = private_key.hex()
-        public_key = SigningKey(mnemonic).verify_key.encode().hex()
+        if (not url and not chain) or (url and chain):
+            raise ValueError("Either 'url' or 'chain' must be provided")
+        elif url:
+            self.url = url
+        elif chain:
+            self.url = chain_url[chain]
+        self.session = requests.Session()
+    
+    def get_version(self):
+        self._version = self.session.get(self.url)
+        return self._version.json()
 
-        return cls(private_key=private_key, hex_private_key=hex_private_key, public_key=public_key)
+    def get_transactions(self, start=None, limit=None):
+        if start:
+            self._transactions = self.session.get(f'{self.url}/transactions?start={start}')
+        elif limit:
+            self._transactions = self.session.get(f'{self.url}/transactions?limit={limit}')
+        elif (start and limit):
+            self._transactions = self.session.get(f'{self.url}/transactions?limit={limit}&start={start}')
+        else:
+            self._transactions = self.session.get(f'{self.url}/transactions')
+        return self._transactions.json()
 
-a = Keypair.create_from_mnemonic('design guide gasp train traffic slight mansion aware notice home cute season')
-print(a)
+    def get_transaction(self, txn_hash_or_version: str or int):
+        self._transaction = self.session.get(f'{self.url}/transactions/{txn_hash_or_version}')
+        return self._transaction.json()
 
+    def get_account_transactions(self, address: str):
+        self._account_transactions = self.session.get(f'{self.url}/accounts/{address}/transactions')
+        return self._account_transactions.json()
+
+    def get_account(self, address: str):
+        self._account = self.session.get(f'{self.url}/accounts/{address}')
+        return self._account.json()
+
+    def get_account_resources(self, address: str):
+        self._account_resources = self.session.get(f'{self.url}/accounts/{address}/resources')
+        return self._account_resources.json()
+
+    def get_account_resource(self, address: str, resource_type: str):
+        self._account_resource = self.session.get(f'{self.url}/accounts/{address}/resource/{resource_type}')
+        return self._account_resource.json()
+
+    def get_account_modules(self, address: str):
+        self._account_modules = self.session.get(f'{self.url}/accounts/{address}/modules')
+        return self._account_modules.json()
+
+    def get_account_module(self, address: str, module_name: str):
+        self._account_module = self.session.get(f'{self.url}/accounts/{address}/module/{module_name}')
+        return self._account_module.json()
+
+    def get_events_by_key(self, event_key: str):
+        self._events = self.session.get(f'{self.url}/events/{event_key}')
+        return self._events.json()
+
+    def get_events_by_handle(self, address: str, event_handle_struct: str, field_name: str):
+        self._events = self.session.get(f'{self.url}/accounts/{address}/events/{event_handle_struct}/{field_name}')
+        return self._events.json()
 
 
     
